1fb4e2a5f37a9e3195379e5650d7c207973780b7
Jovani Aparecido Florencio <jovanism@hotmail.com>
2023-09-13 15:27:58 -0300

    alteração com o operador ternario

---
+++ tipos_variaveis/src/operador_ternario.java
@@ -0,0 +1,18 @@
+public class operador_ternario {
+    public static void main(String[] args){
+        int a,b;
+
+        a=5;
+        b=6;
+        String resultado = a==b ? "verdadeiro" : "falso";
+        /* modelo tradicional co if
+        if(a==b)
+            resultado = "verdadeiro";
+            else
+                resultado = "falso";
+                System.out.println(resultado);*/
+
+            System.out.println(resultado);
+
+    }
+}
