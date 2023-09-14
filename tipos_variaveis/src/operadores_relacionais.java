32ca38287cf6ac7fc076ca50f7c76c6567560b4a
Jovani Aparecido Florencio <jovanism@hotmail.com>
2023-09-13 22:44:28 -0300

    exercicios de sitaxe java basico

---
+++ tipos_variaveis/src/operadores_relacionais.java
@@ -0,0 +1,22 @@
+public class operadores_relacionais {
+    public static void main(String[] args){
+String nomeUm = "Jovani";
+String nomeDois = "Jovani";
+
+System.out.println(nomeUm.equals(nomeDois));
+//utiliza o .equals para comparar dois objetos
+
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+        System.out.println("numeroUm é igual a numeroDois ?" + simNao);
+
+        /*  boolean simNao = numero1 != numero2;
+        System.out.println("numeroUm é igual a numeroDois ?" + simNao);
+
+        boolean simNao = numero1 > numero2;
+        System.out.println("numeroUm é igual a numeroDois ?" + simNao);
+   */
+    }
+}
