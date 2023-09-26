e3bca8d7e24a0aac2b01e5b1474f9339ff4b4e21
Jovani Aparecido Florencio <jovanism@hotmail.com>
2023-09-26 15:19:58 -0300

    desafio sintaxe banco-terminal

--- ContaBanco/ContaTerminal/src/ContaTerminal.java
+++ ContaBanco/ContaTerminal/src/ContaTerminal.java
@@ -1,15 +1,16 @@
+//importei a classe Scanner
 import java.util.Scanner;
 
 public class ContaTerminal {
 
 	public static void main(String[] args) {
-
+//chamei a classe Scanner e criei as variaveis
 		Scanner sc = new Scanner(System.in);
 		int numero;
 		String agencia;
 		String nomeCliente;
 		double saldo;
-
+//aqui sera solicitado que se digite as informações no terminal
 		System.out.println("---------------ABRA A SUA CONTA BANCÁRIA---------------");
 		System.out.println();
 		System.out.println("Digite o número da Conta:");
@@ -24,7 +25,7 @@ public class ContaTerminal {
 
 		System.out.println("Deposite um valor para a abertura da sua conta: ");
 		saldo = sc.nextDouble();
-
+// aqui sera exibido as informações digitadas
 		System.out.println();
 		System.out.printf("Olá %s, obrigado por criar uma conta em " + "nosso banco, sua agência é %s, " + 
         "conta %d e seu saldo %.2f já está disponível" + " para saque.", nomeCliente, agencia, numero, saldo);
